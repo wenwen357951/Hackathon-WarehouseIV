d327d5052054e74b87be741e1afc0fa03f582100
john <4a7g0018@stust.edu.tw>
2021-07-14 17:37:00 +0800

    +add HowItWork blade +add HowItWork Img

---
+++ Hackathon-WarehouseIV/resources/views/component/HowItWork.blade.php
@@ -0,0 +1,38 @@
+<link href="../../css/intent.css" rel="stylesheet">
+
+<style>
+    .item .img-fluid{
+        width: 400px;
+        height: 300px;
+    }
+    col{
+        background-color: lightskyblue;
+    }
+    center{
+        background-color: lightskyblue;
+    }
+    .item{
+        padding-top:100px ;
+    /background-color: lightskyblue;/
+    }
+</style>
+
+<div class="Howitwork">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-4" >
+                <div class="item">
+                    <img src="img/HowItWork.jpg" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-6 col-md-6">
+                <div class="item">
+                    <h1>How it work</h1>
+                    <h4>我們是如何運作的呢<br>我們根據聯合國永續發展目標中的消除貧窮，為你創立了許多募資的項目分別為食物募資、教育資源募資、金錢募資、醫療募資、基礎建設募資。這些募資項目不限於金錢上的捐助，可以依照個人或企業想要贊助的項目去做募資，我們會把這些資源整合起來，提供給世界上有所需要的人!並且把使用項目公開透明。
+                    </h4>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
