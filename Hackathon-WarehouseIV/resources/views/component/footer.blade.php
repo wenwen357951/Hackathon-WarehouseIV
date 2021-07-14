cd3d91cfdc7dbd0c871dc562d916a03a5edd76d0
john <4a7g0018@stust.edu.tw>
2021-07-14 23:02:46 +0800

    +add footer blade

---
+++ Hackathon-WarehouseIV/resources/views/component/footer.blade.php
@@ -0,0 +1,30 @@
+<style>
+    .footer .container-fluid{
+        background: gray;
+        color: white;
+        font-weight: 600;
+        z-index: 100;
+    }
+
+
+    .address{
+        text-align: center;
+        padding: 10px;
+        margin-bottom: 10px;
+        z-index: 1;
+    }
+
+    .Email{
+        text-align: center;
+        padding-bottom: 10px;
+        z-index: 1;
+    }
+
+</style>
+
+<div class="footer">
+    <div class="container-fluid">
+        <div class="address">台南市永康區南台街1號　No. 1 , Nantai St. , Youngkang Dist. , Taiwan City 710301 , Taiwan(R.O.C)</div>
+        <div class="Email">Email : houbonbon@houbonbon.mail.com</div>
+    </div>
+</div>
